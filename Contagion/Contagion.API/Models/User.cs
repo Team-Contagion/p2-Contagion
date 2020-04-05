bb38ab754a16b5356271fce04e001c733d248200
Christian-Bennett <61510916+Christian-Bennett@users.noreply.github.com>
2020-04-05 13:18:26 -0500

    Update User.cs

--- Contagion/Contagion.API/Models/User.cs
+++ Contagion/Contagion.API/Models/User.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using Contagion.API.Repositories;
+using Contagion.Storage.Repositories;
 
 namespace Contagion.API.Models
 {
