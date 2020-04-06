7cbc367d9ef72cab6e7106dbc47b4154b5674d67
Christian-Bennett <61510916+Christian-Bennett@users.noreply.github.com>
2020-04-05 20:57:35 -0500

    Test

--- Contagion/Contagion.API/Controllers/ContagionController.cs
+++ Contagion/Contagion.API/Controllers/ContagionController.cs
@@ -11,30 +11,12 @@ namespace Contagion.API.Controllers
 {
   [ApiController]
   [Route("[controller]")]
-  public class ContagionController : ControllerBase
+  public class ContagionController : Controller
   {
-    private static readonly List<ContagionModel> _fml = new List<ContagionModel>()
+    [HttpPost]
+    public IActionResult NewUser()
     {
-      new ContagionModel() { Title = "Watchmen" },
-      new ContagionModel() { Title = "Forest Gump" },
-      new ContagionModel() { Title = "Test" },
-      new ContagionModel() { Title = "Test 2" },
-      new ContagionModel() { Title = "Test 3" },
-      new ContagionModel() { Title = "Test 4" }
-    };
-
-    private readonly ILogger<ContagionController> _logger;
-
-    public ContagionController(ILogger<ContagionController> logger)
-    {
-      _logger = logger;
-    }
-
-    public IActionResult Get()
-    {
-      var res = (new HttpClient()).GetAsync("https://swapi.co/").GetAwaiter().GetResult();
-      var films = res.Content.ReadAsStringAsync();
-      return Ok(_fml);
+      return View();
     }
   }
 }
